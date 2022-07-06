6b1c1ed0937129c89689f17f92d58f8739665794
soskaris96 <sospeterkariuki2008@gmail.com>
2022-07-06 14:56:07 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,6 +8,5 @@
  * Return: returns 1 and 0 depending on the condition given
  */
 int _isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+ii}
